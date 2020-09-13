721435f46f40e22cf3feb800b000c0c32afb3035
Leo Okumura <leo.okumura@gmail.com>
2020-09-13 16:03:24 -0300

    Tratamento marcas indefinidas

--- src/components/atoms/ProductBrand/index.js
+++ src/components/atoms/ProductBrand/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProductBrandStyled from "./styles";
 
 const ProductBrand = ({ brand }) => (
-  <ProductBrandStyled>{`marca: ${brand}`}</ProductBrandStyled>
+  <ProductBrandStyled>{`marca: ${brand || ""}`}</ProductBrandStyled>
 );
 
 export default ProductBrand;
