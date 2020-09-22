05abeb900d689e55828c5b9c765711798596d407
Leo Okumura <leo.okumura@gmail.com>
2020-09-22 13:34:27 -0300

    Formulario endereco ativo so quando nao achou

--- src/components/molecules/Review/index.js
+++ src/components/molecules/Review/index.js
@@ -25,7 +25,7 @@ const Review = ({ handleNext }) => {
   } = useForm({ mode: "onBlur" });
   const [shippingType, setShippingType] = useState();
   const [userAddresses, setUserAddresses] = useState([]);
-  const [cartAddress, setCartAddress] = useState();
+  const [cartAddress, setCartAddress] = useState(null);
 
   const cep = watch("zip", 0);
   const hasZipLength = cep.toString().length === 9;
@@ -139,202 +139,207 @@ const Review = ({ handleNext }) => {
 
         <Grid xs={12}>{validationCep && <ValidationAddress />}</Grid>
 
-        <Grid xs={12} sm={12}>
-          <TitleStyles>novo endereço</TitleStyles>
-        </Grid>
+        {!cartAddress && (
+          <Grid xs={12} sm={12}>
+            <TitleStyles>novo endereço</TitleStyles>
+          </Grid>
+        )}
       </Grid>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={3}>
-          <Grid xs={12} sm={8}>
-            <label htmlFor="CEP">CEP*</label>
-            <InputAlternative
-              ref={register({ required: true, minLength: 9 })}
-              invert={true}
-              mask="maskCep"
-              qa="account_input_cep"
-              width={50}
-              config={{
-                type: "text",
-                id: "zip",
-                name: "zip",
-                placeholder: "Digite apenas números",
-                error: errors.zip,
-                message:
-                  errors.zip && errors.zip.types && errors.zip.types.notFound
-                    ? "CEP não encontrado"
-                    : "Este é um campo obrigatório.",
-              }}
-            />
-          </Grid>
-          <Grid xs={12} sm={8}>
-            <label htmlFor="street">Rua*</label>
-            <InputAlternative
-              ref={register({ required: true })}
-              invert={true}
-              width={100}
-              config={{
-                type: "text",
-                id: "street",
-                name: "street",
-                error: errors.street,
-                message: "Este é um campo obrigatório.",
-              }}
-            />
-          </Grid>
-
-          <Grid xs={6} sm={4}>
-            <label htmlFor="number">Número*</label>
-            <InputAlternative
-              ref={register({ required: true })}
-              invert={true}
-              width={100}
-              config={{
-                type: "text",
-                id: "number",
-                name: "number",
-                error: errors.number,
-                message: "Este é um campo obrigatório.",
-              }}
-            />
-          </Grid>
-
-          <Hidden only={["lg", "sm"]}>
-            <Grid xs={6}>
-              <label htmlFor="neighborhood">Bairro*</label>
+      {!cartAddress && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container spacing={3}>
+            <Grid xs={12} sm={8}>
+              <label htmlFor="CEP">CEP*</label>
+              <InputAlternative
+                ref={register({ required: true, minLength: 9 })}
+                invert={true}
+                mask="maskCep"
+                qa="account_input_cep"
+                width={50}
+                config={{
+                  type: "text",
+                  id: "zip",
+                  name: "zip",
+                  placeholder: "Digite apenas números",
+                  error: errors.zip,
+                  message:
+                    errors.zip && errors.zip.types && errors.zip.types.notFound
+                      ? "CEP não encontrado"
+                      : "Este é um campo obrigatório.",
+                }}
+              />
+            </Grid>
+            <Grid xs={12} sm={8}>
+              <label htmlFor="street">Rua*</label>
               <InputAlternative
                 ref={register({ required: true })}
                 invert={true}
                 width={100}
                 config={{
                   type: "text",
-                  id: "neighborhood",
-                  name: "neighborhood",
-                  error: errors.neighborhood,
+                  id: "street",
+                  name: "street",
+                  error: errors.street,
                   message: "Este é um campo obrigatório.",
                 }}
               />
             </Grid>
-          </Hidden>
 
-          <Grid xs={12} sm={8}>
-            <label htmlFor="complemento">Complemento</label>
-            <InputAlternative
-              ref={register}
-              invert={true}
-              width={100}
-              config={{
-                type: "text",
-                id: "complemento",
-                name: "complemento",
-                error: errors.complemento,
-                message: "Este é um campo obrigatório.",
-              }}
-            />
-          </Grid>
+            <Grid xs={6} sm={4}>
+              <label htmlFor="number">Número*</label>
+              <InputAlternative
+                ref={register({ required: true })}
+                invert={true}
+                width={100}
+                config={{
+                  type: "text",
+                  id: "number",
+                  name: "number",
+                  error: errors.number,
+                  message: "Este é um campo obrigatório.",
+                }}
+              />
+            </Grid>
 
-          <Hidden only="xs">
+            <Hidden only={["lg", "sm"]}>
+              <Grid xs={6}>
+                <label htmlFor="neighborhood">Bairro*</label>
+                <InputAlternative
+                  ref={register({ required: true })}
+                  invert={true}
+                  width={100}
+                  config={{
+                    type: "text",
+                    id: "neighborhood",
+                    name: "neighborhood",
+                    error: errors.neighborhood,
+                    message: "Este é um campo obrigatório.",
+                  }}
+                />
+              </Grid>
+            </Hidden>
+
+            <Grid xs={12} sm={8}>
+              <label htmlFor="complemento">Complemento</label>
+              <InputAlternative
+                ref={register}
+                invert={true}
+                width={100}
+                config={{
+                  type: "text",
+                  id: "complemento",
+                  name: "complemento",
+                  error: errors.complemento,
+                  message: "Este é um campo obrigatório.",
+                }}
+              />
+            </Grid>
+
+            <Hidden only="xs">
+              <Grid xs={12} sm={4}>
+                <label htmlFor="neighborhood">Bairro*</label>
+                <InputAlternative
+                  ref={register({ required: true })}
+                  invert={true}
+                  width={100}
+                  config={{
+                    type: "text",
+                    id: "neighborhood",
+                    name: "neighborhood",
+                    error: errors.neighborhood,
+                    message: "Este é um campo obrigatório.",
+                  }}
+                />
+              </Grid>
+            </Hidden>
+
+            <Grid xs={6} sm={8}>
+              <label htmlFor="cidade">Cidade*</label>
+              <InputAlternative
+                ref={register({ required: true })}
+                invert={true}
+                width={100}
+                config={{
+                  type: "text",
+                  id: "cidade",
+                  name: "cidade",
+                  error: errors.cidade,
+                  message: "Este é um campo obrigatório.",
+                }}
+              />
+            </Grid>
+            <Grid xs={6} sm={4}>
+              <label htmlFor="estado">Estado*</label>
+              <InputAlternative
+                ref={register({ required: true })}
+                invert={true}
+                width={100}
+                config={{
+                  type: "text",
+                  id: "estado",
+                  name: "estado",
+                  error: errors.estado,
+                  message: "Este é um campo obrigatório.",
+                }}
+              />
+            </Grid>
             <Grid xs={12} sm={4}>
-              <label htmlFor="neighborhood">Bairro*</label>
+              <label htmlFor="firstname">Nome*</label>
               <InputAlternative
                 ref={register({ required: true })}
                 invert={true}
-                width={100}
+                mask="maskName"
                 config={{
                   type: "text",
-                  id: "neighborhood",
-                  name: "neighborhood",
-                  error: errors.neighborhood,
+                  id: "firstname",
+                  name: "firstname",
+                  error: errors.firstname,
+                  message: "Entre com um nome válido",
+                }}
+              />
+            </Grid>
+            <Grid xs={12} sm={5}>
+              <label htmlFor="lastname">Sobrenome*</label>
+              <InputAlternative
+                ref={register({ required: true })}
+                invert={true}
+                mask="maskName"
+                qa="account_input_lastname"
+                config={{
+                  type: "text",
+                  id: "lastname",
+                  name: "lastname",
+                  error: errors.lastname,
+                  message: "Entre com um nome válido",
+                }}
+              />
+            </Grid>
+            <Grid xs={12} sm={3}>
+              <label htmlFor="tel">Telefone</label>
+              <InputAlternative
+                ref={register}
+                mask="maskTelCel"
+                config={{
+                  type: "text",
+                  id: "telephone",
+                  name: "telephone",
+                  placeholder: "DDD + Telefone",
+                  error: errors.telephone,
                   message: "Este é um campo obrigatório.",
                 }}
               />
             </Grid>
-          </Hidden>
+          </Grid>
 
-          <Grid xs={6} sm={8}>
-            <label htmlFor="cidade">Cidade*</label>
-            <InputAlternative
-              ref={register({ required: true })}
-              invert={true}
-              width={100}
-              config={{
-                type: "text",
-                id: "cidade",
-                name: "cidade",
-                error: errors.cidade,
-                message: "Este é um campo obrigatório.",
-              }}
-            />
+          <Grid container spacing={3} justify="flex-end">
+            <Grid xs={12} sm={6} className="submit" className="button">
+              <input type="submit" value="cadastrar endereço" />
+            </Grid>
           </Grid>
-          <Grid xs={6} sm={4}>
-            <label htmlFor="estado">Estado*</label>
-            <InputAlternative
-              ref={register({ required: true })}
-              invert={true}
-              width={100}
-              config={{
-                type: "text",
-                id: "estado",
-                name: "estado",
-                error: errors.estado,
-                message: "Este é um campo obrigatório.",
-              }}
-            />
-          </Grid>
-          <Grid xs={12} sm={4}>
-            <label htmlFor="firstname">Nome*</label>
-            <InputAlternative
-              ref={register({ required: true })}
-              invert={true}
-              mask="maskName"
-              config={{
-                type: "text",
-                id: "firstname",
-                name: "firstname",
-                error: errors.firstname,
-                message: "Entre com um nome válido",
-              }}
-            />
-          </Grid>
-          <Grid xs={12} sm={5}>
-            <label htmlFor="lastname">Sobrenome*</label>
-            <InputAlternative
-              ref={register({ required: true })}
-              invert={true}
-              mask="maskName"
-              qa="account_input_lastname"
-              config={{
-                type: "text",
-                id: "lastname",
-                name: "lastname",
-                error: errors.lastname,
-                message: "Entre com um nome válido",
-              }}
-            />
-          </Grid>
-          <Grid xs={12} sm={3}>
-            <label htmlFor="tel">Telefone</label>
-            <InputAlternative
-              ref={register}
-              mask="maskTelCel"
-              config={{
-                type: "text",
-                id: "telephone",
-                name: "telephone",
-                placeholder: "DDD + Telefone",
-                error: errors.telephone,
-                message: "Este é um campo obrigatório.",
-              }}
-            />
-          </Grid>
-        </Grid>
+        </form>
+      )}
 
-        <Grid container spacing={3} justify="flex-end">
-          <Grid xs={12} sm={6} className="submit" className="button">
-            <input type="submit" value="cadastrar endereço" />
-          </Grid>
-        </Grid>
-      </form>
     </ReviewStyles>
   );
 };
