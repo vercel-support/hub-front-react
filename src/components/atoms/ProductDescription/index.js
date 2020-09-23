69535a5df3f7e9cea9036ea3769836a7324f238c
Leo Okumura <leo.okumura@gmail.com>
2020-09-23 10:01:30 -0300

    Forcando html na pdp

--- src/components/atoms/ProductDescription/index.js
+++ src/components/atoms/ProductDescription/index.js
@@ -1,9 +1,10 @@
 import React from "react";
-import parse from "html-react-parser";
 import ProductDescriptionStyled from "./styles";
 
 const ProductDescription = ({ description }) => (
-  <ProductDescriptionStyled>{parse(description)}</ProductDescriptionStyled>
+  <ProductDescriptionStyled dangerouslySetInnerHTML={{__html: description}} />
 );
 
+
+ 
 export default ProductDescription;
