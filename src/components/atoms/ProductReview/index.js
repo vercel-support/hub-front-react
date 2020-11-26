c1a43884363c9d421dcedffbbb2c3cd20ea2190a
Marco Domingues <marco.dominguesjr@gmail.com>
2020-11-26 00:16:07 -0300

    Yourviews pdp

---
+++ src/components/atoms/ProductReview/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProductReview = () => (
+    <>
+        <div id="yv-reviews"></div>
+        <div class="yv-qa"></div>
+    </>
+);
+
+export default ProductReview;
